8efeaf654592d231c33105ab8a420c1c1b414646
Maxim <76247935+MaxNoragami@users.noreply.github.com>
2024-09-05 13:10:09 +0300

    Delete newproject directory

--- newproject/api/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-
-from .models import Passwords
-
-class PasswordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Passwords
-        fields = '__all__'
