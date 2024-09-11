44fa9f162770cc59fa8503fff4bb6932ce2a6022
MaxNoragami <maxcraftmd@gmail.com>
2024-09-12 00:23:43 +0300

    Fixed the GET request, now only the password-items related to the specified groupId will return

--- locker_backend/api/password_items_view_set.py
+++ locker_backend/api/password_items_view_set.py
@@ -3,8 +3,9 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from api.models import PasswordItems, Groups, PasswordHistory
+from api.models import PasswordItems, Groups
 from api.serializers import PasswordItemSerializer
+from rest_framework.exceptions import ValidationError
 
 class PasswordItemsViewSet(viewsets.ModelViewSet):
     queryset = PasswordItems.objects.all()
@@ -12,12 +13,15 @@ class PasswordItemsViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # Get only the password items for the logged-in user
+        # Filter password items by the authenticated user and groupId (if provided)
         user = self.request.user
+        group_id = self.kwargs.get('groups_pk')
+        if group_id:
+            return PasswordItems.objects.filter(userId=user, groupId=group_id)
         return PasswordItems.objects.filter(userId=user)
 
     def list(self, request, *args, **kwargs):
-        # Override the list method to return the filtered queryset
+        # Override the list method to return filtered password items
         queryset = self.get_queryset()
         serializer = PasswordItemSerializer(queryset, many=True)
         return Response(serializer.data)
@@ -30,50 +34,42 @@ class PasswordItemsViewSet(viewsets.ModelViewSet):
         serializer = PasswordItemSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    @action(methods=['get'], detail=False)
-    def get_password_items_by_location(self, request, groups_pk=None):
-        group_location = self.request.query_params.get('group_location')
-        if group_location is not None:
-            password_items = PasswordItems.objects.filter(
-                groupId__groupId=groups_pk,
-                groupId__groupName=group_location
-            )
-            serializer = PasswordItemSerializer(password_items, many=True)
-            return Response(serializer.data)
-        return Response({"detail": "Group location not provided"}, status=status.HTTP_400_BAD_REQUEST)
-
     @action(methods=['post'], detail=True)
-    def create_password_item(self, request, groups_pk = None):
+    def create_password_item(self, request, groups_pk=None):
+        # Force the groupId to match the one in the URL
+        request.data['groupId'] = groups_pk  # Override the groupId in the request data
+
+        # Proceed with the creation using the corrected groupId
         serializer = PasswordItemSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
     @action(methods=['get'], detail=True)
-    def get_specific_password_items(self, request, pk = None, groups_pk = None):
-        queryset = PasswordItems.objects.filter(pk=pk, groups=groups_pk)
+    def get_specific_password_items(self, request, pk=None, groups_pk=None):
+        # Retrieve a specific password item within a specific group
+        queryset = PasswordItems.objects.filter(pk=pk, groupId=groups_pk)
         password_items = get_object_or_404(queryset, pk=pk)
         serializer = PasswordItemSerializer(password_items)
         return Response(serializer.data)
 
     @action(methods=['put'], detail=True)
     def put_password_items(self, request, pk=None, groups_pk=None):
+        # Update a specific password item in a group
         queryset = PasswordItems.objects.filter(pk=pk, groupId=groups_pk)
         password_items = get_object_or_404(queryset, pk=pk)
 
         serializer = PasswordItemSerializer(password_items, data=request.data)
         if serializer.is_valid():
-            serializer.save()  # This will trigger the save method in the model
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
     @action(methods=['delete'], detail=True)
-    def delete_password_items(self, request, pk=None, groups_pk = None):
-        queryset = PasswordItems.objects.filter(pk=pk, groups=groups_pk)
+    def delete_password_items(self, request, pk=None, groups_pk=None):
+        # Delete a specific password item in a group
+        queryset = PasswordItems.objects.filter(pk=pk, groupId=groups_pk)
         password_items = get_object_or_404(queryset, pk=pk)
-
         password_items.delete()
