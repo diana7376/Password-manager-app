c8c795fc108e87cb1ba2bf17290678e76b369679
Daniel Cojocaru <danielcojcoaru2005@gmail.com>
2024-09-25 13:32:11 +0300

    Fixed the item_name duplication bug

--- locker_backend/api/serializers.py
+++ locker_backend/api/serializers.py
@@ -47,7 +47,13 @@ class PasswordItemSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        validated_data['user'] = self.context['request'].user
+        user = self.context['request'].user
+        item_name = validated_data['item_name']
+
+        if PasswordItems.objects.filter(user= user, item_name= item_name):
+            raise serializers.ValidationError({'item_name' : 'A PasswordItem with this name already exists.'})
+
+        validated_data['user'] = user
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
